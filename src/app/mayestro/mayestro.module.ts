bf526c10b6befff05ddbf8cddb08db1c05ce3efa
Ahmed Helmy <ahmhelmhalm@gmail.com>
2020-03-12 20:54:17 +0200

    mayestro layout

--- src/app/mayestro/mayestro.module.ts
+++ src/app/mayestro/mayestro.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MayestroRoutingModule } from './mayestro-routing.module';
 import { MayestroComponent } from './mayestro.component';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { PlayerComponent } from './player/player.component';
 
 
 @NgModule({
-  declarations: [MayestroComponent],
+  declarations: [MayestroComponent, HeaderComponent, SideBarComponent, PlayerComponent],
   imports: [
     CommonModule,
     MayestroRoutingModule
