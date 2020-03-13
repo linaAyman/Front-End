2411f49166cdcc9201eea3c7194e7c947a985390
Ahmed Helmy <ahmhelmhalm@gmail.com>
2020-03-13 16:17:00 +0200

    generate signin and signup components

--- src/app/account-mayestro/account-mayestro.module.ts
+++ src/app/account-mayestro/account-mayestro.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AccountMayestroRoutingModule } from './account-mayestro-routing.module';
 import { AccountMayestroComponent } from './account-mayestro.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
-  declarations: [AccountMayestroComponent],
+  declarations: [AccountMayestroComponent, SigninComponent, SignupComponent],
   imports: [
     CommonModule,
     AccountMayestroRoutingModule
