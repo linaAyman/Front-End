e744db6eaec49c0b105ceb23cd166478a060123e
linaAyman <45544470+linaAyman@users.noreply.github.com>
2020-03-28 15:48:45 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -xe
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i testaws.pem ubuntu@ec2-3-17-150-160.us-east-2.compute.amazonaws.com << EOF
 rm -rf Front-End
 git clone https://github.com/linaAyman/Front-End.git
-cd Front_End
+cd Front-End
 npm install
 npm install -g @angular/cli@8.3.5 
 rm -rf dist
