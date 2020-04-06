0aea651e3cb7c34bdbb0db2167b89c9f50944648
Ahmed Helmy <ahmhelmhalm@gmail.com>
2020-04-06 15:37:57 +0200

    test

--- src/app/mayestro/player/player.component.ts
+++ src/app/mayestro/player/player.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AccountMayestroService } from "src/app/account-mayestro/account-mayestro.service";
 
 @Component({
-  selector: 'app-player',
-  templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  selector: "app-player",
+  templateUrl: "./player.component.html",
+  styleUrls: ["./player.component.css"]
 })
 export class PlayerComponent implements OnInit {
-
-  constructor() { }
+  constructor(private acc: AccountMayestroService) {}
 
   ngOnInit() {
+    this.acc
+      .checkEmailExist("ahmed@gmail.com")
+      .subscribe(console.log, console.log);
   }
-
 }
