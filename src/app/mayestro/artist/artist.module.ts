d5dad84c0d53c6a8b985bc5a677ea7190af8cc8b
alaaosamaa <youremail@domain.com>
2020-04-07 14:46:57 +0200

    works

--- src/app/mayestro/artist/artist.module.ts
+++ src/app/mayestro/artist/artist.module.ts
@@ -1,12 +1,12 @@
-import { ArtistSongComponent } from './../artist-song/artist-song.component';
+import { ArtistSongComponent } from './artist-song/artist-song.component';
 import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtistRoutingModule } from './artist-routing.module';
 import { RelatedArtistsComponent } from './related-artists/related-artists.component';
 import { AboutArtistComponent } from './about-artist/about-artist.component';
-import { ArtistHeaderComponent } from 'src/app/shared/components/artist-header/artist-header.component';
-import { ArtistCardComponent } from '../artist-card/artist-card.component';
+import { ArtistHeaderComponent } from 'src/app/mayestro/artist/artist-header/artist-header.component';
+import { ArtistCardComponent } from './artist-card/artist-card.component';
 import { ArtistOverviewComponent } from './artist-overview/artist-overview.component';
 import { ArtistComponent } from './artist.component';
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
-import { MiniCardViewerComponent } from './components/mini-card-viewer/mini-card-viewer.component';
-import { ArtistHeaderComponent } from './components/artist-header/artist-header.component';
+import { MiniCardViewerComponent } from '../mayestro/mini-card-viewer/mini-card-viewer.component';
+import { ArtistHeaderComponent } from '../mayestro/artist/artist-header/artist-header.component';
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { fakeBackendProvider } from './mock-server/mock-server.service';
-import { CardComponent } from './components/card/card.component';
-import { HomeArtistCardComponent } from './components/home-artist-card/home-artist-card.component';
+import { CardComponent } from '../mayestro/card/card.component';
+import { HomeArtistCardComponent } from '../mayestro/home-artist-card/home-artist-card.component';
 
 
 
