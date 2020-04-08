b879b51f6bddaf4ff1dcfa294527e7f86d3378b2
Ahmed Helmy <ahmhelmhalm@gmail.com>
2020-04-08 18:05:48 +0200

    Mock-Serve Data for Testing team

--- src/app/mayestro/track/track.component.ts
+++ src/app/mayestro/track/track.component.ts
@@ -1,44 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { MayestroService } from '../mayestro.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { MayestroService } from "../mayestro.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-track',
-  templateUrl: './track.component.html',
-  styleUrls: ['./track.component.css']
+  selector: "app-track",
+  templateUrl: "./track.component.html",
+  styleUrls: ["./track.component.css"]
 })
 export class TrackComponent implements OnInit {
-  songs:any[]
-  isPlaying=false;
-  id:any;
-  type:string;
-  constructor(private service: MayestroService, private route: ActivatedRoute) { }
+  songs: any[];
+  isPlaying = false;
+  id: any;
+  type: string;
+  constructor(
+    private service: MayestroService,
+    private route: ActivatedRoute
+  ) {}
 
-  
   ngOnInit() {
-    this.route.params.subscribe(param=>this.id=param["id"],
-    param=>this.type=param["type"])
-    if(this.type=="album"){
-      this.service.getTracks(this.id)
-      .subscribe(
-        (data:any[]) =>{
-          this.songs=data;
-        });
-    }
-    if(this.type=="playlist"){
-      this.service.getPlaylistTracks(this.id)
-      .subscribe(
-        (data:any[]) =>{
-          this.songs=data;
-        });
-    }
-    
+    // this.route.params.subscribe(param=>this.id=param["id"],
+    // param=>this.type=param["type"])
+    // if(this.type=="album"){
+    //   this.service.getTracks(this.id)
+    //   .subscribe(
+    //     (data:any[]) =>{
+    //       this.songs=data;
+    //     });
+    // }
+    // if(this.type=="playlist"){
+    //   this.service.getPlaylistTracks(this.id)
+    //   .subscribe(
+    //     (data:any[]) =>{
+    //       this.songs=data;
+    //     });
+    // }
   }
-  play()
-  {
+  play() {
     console.log("playing");
-    if(this.isPlaying==true)
-       this.isPlaying=false;
-    else this.isPlaying=true;
-  }  
+    if (this.isPlaying == true) this.isPlaying = false;
+    else this.isPlaying = true;
+  }
 }
