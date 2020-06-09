b623d19073f1386faaeb24d066358440c6d2f086
alaaosamaa <youremail@domain.com>
2020-06-10 00:35:24 +0200

    integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component, OnInit, OnDestroy, HostListener } from "@angular/core";
 })
 export class AppComponent {
   title = 'Maestro';
+
+  /**
+   * function to go to the top of window 
+   * @param event event get from html
+   */
+  onActivate(event) {
+    let scrollToTop = window.setInterval(() => {
+        let pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 20); // how far to scroll on each step
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 16);}
 }
