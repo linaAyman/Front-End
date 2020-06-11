30f8021e7e3dccb81ac0a2045a039ddf6ef0a4e0
Ahmed Helmy <ahmhelmhalm@gmail.com>
2020-06-11 14:50:24 +0200

    p

--- src/app/mayestro/artists/artists.component.ts
+++ src/app/mayestro/artists/artists.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { MayestroService } from '../mayestro.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { MayestroService } from "../mayestro.service";
 // import { ArtistService } from '../artist/artist.service';
-import { ActivatedRoute } from '@angular/router';
-import { LikeAndFollowService } from '../like-and-follow.service';
-import { ICard } from '../card/card.interface';
+import { ActivatedRoute } from "@angular/router";
+import { LikeAndFollowService } from "../like-and-follow.service";
+import { ICard } from "../card/card.interface";
 @Component({
-  selector: 'app-artists',
-  templateUrl: './artists.component.html',
-  styleUrls: ['./artists.component.css']
+  selector: "app-artists",
+  templateUrl: "./artists.component.html",
+  styleUrls: ["./artists.component.css"],
 })
 export class ArtistsComponent implements OnInit {
-   FollowedArtists:any[];
-   length:any;
-  constructor( private route:ActivatedRoute,private mayestro:MayestroService, private followartist : LikeAndFollowService) { }
+  FollowedArtists: any[];
+  length: any;
+  constructor(
+    private route: ActivatedRoute,
+    private mayestro: MayestroService,
+    private followartist: LikeAndFollowService
+  ) {}
 
   ngOnInit() {
     /**
      * gets followed artists
      */
-    this.followartist.GetFollowedArtists()
-    .subscribe((comp:any)=>{     
-    this.FollowedArtists=comp[0].artists;
-    this.length=this.FollowedArtists.length;
+    this.followartist.GetFollowedArtists().subscribe((comp: any) => {
+      if (comp) {
+        this.FollowedArtists = comp[0].artists;
+        this.length = this.FollowedArtists.length;
+      } else {
+        this.length = 0;
+      }
     });
-  
-    }
-   
+  }
 }
